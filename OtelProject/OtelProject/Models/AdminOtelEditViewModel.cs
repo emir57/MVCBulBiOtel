cdbab140887310f5861f21191922d5c923e9543b
emir57 <emir.gurbuz06@hotmail.com>
2022-06-24 15:03:10 +0300

    Create AdminOtelEditViewModel

---
+++ OtelProject/OtelProject/Models/AdminOtelEditViewModel.cs
@@ -0,0 +1,11 @@
+﻿using OtelProject.Models.Tables;
+using System.Collections.Generic;
+
+namespace OtelProject.Models
+{
+    public class AdminOtelEditViewModel
+    {
+        public List<Country> Countries { get; set; }
+        public Otel Otel { get; set; }
+    }
+}
