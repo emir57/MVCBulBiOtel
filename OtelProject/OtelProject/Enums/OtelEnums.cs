eccc47aa452c54b806fc17e1bb6e183457117fc6
emir57 <emir.gurbuz06@hotmail.com>
2022-06-24 16:24:44 +0300

    Create OtelStatus Enum

---
+++ OtelProject/OtelProject/Enums/OtelEnums.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OtelProject.Enums
+{
+    public enum OtelStatus
+    {
+        Wait = 0,
+        No = 1,
+        Ok = 3
+    }
+}
